56b63ccc418aaaaedd234b52a029402cb83becac
Stefan Schneider <stefan.schneider@upb.de>
2018-02-02 15:27:56 +0100

    fixed docker build

--- pipeline/build/build.sh
+++ pipeline/build/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -f ../../DockerfilePipeline -t tng-sdk-descriptorgen:pipeline ../../.
+docker build -f DockerfilePipeline -t registry.sonata-nfv.eu:5000/tng-sdk-descriptorgen:test .
 
