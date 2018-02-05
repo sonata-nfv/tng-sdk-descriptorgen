7477e14aea27b749c32f2b7564fc0dd919ffca76
Stefan Schneider <stefan.schneider@upb.de>
2018-02-05 11:29:30 +0100

    restored test script

--- pipeline/unittest/test.sh
+++ pipeline/unittest/test.sh
@@ -3,14 +3,13 @@
 # immediately exit after an error
 set -e
 
-docker stop descriptorgen
-#sudo docker rm descriptorgen
+#docker stop descriptorgen
 
-#docker run --name descriptorgen --rm -i -d 5gtango/tng-sdk-descriptorgen:test
-#echo "Test container running"
-#sleep 1
-#docker exec -i -d descriptorgen webdriver-manager start
-#echo "Webdriver-manager starting (for protractor unit tests)"
-#sleep 3
-#docker exec -i descriptorgen protractor tng-sdk-descriptorgen/pipeline/unittest/conf.js
+docker run --name descriptorgen --rm -i -d 5gtango/tng-sdk-descriptorgen:test
+echo "Test container running"
+sleep 1
+docker exec -i -d descriptorgen webdriver-manager start
+echo "Webdriver-manager starting (for protractor unit tests)"
+sleep 3
+docker exec -i descriptorgen protractor tng-sdk-descriptorgen/pipeline/unittest/conf.js
 
