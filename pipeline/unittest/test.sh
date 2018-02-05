053ddfa3483d6eeaeba3d52e9ff188e15fce0c75
Stefan Schneider <stefan.schneider@upb.de>
2018-02-05 11:19:38 +0100

    trying to fix 'not a tty device' error

--- pipeline/unittest/test.sh
+++ pipeline/unittest/test.sh
@@ -3,11 +3,11 @@
 # immediately exit after an error
 set -e
 
-docker run --name descriptorgen --rm -i -d -it 5gtango/tng-sdk-descriptorgen:test
+docker run --name descriptorgen --rm -i -d 5gtango/tng-sdk-descriptorgen:test
 echo "Test container running"
 sleep 1
-docker exec -i -d -it descriptorgen webdriver-manager start
+docker exec -i -d descriptorgen webdriver-manager start
 echo "Webdriver-manager starting (for protractor unit tests)"
 sleep 3
-docker exec -i -it descriptorgen protractor tng-sdk-descriptorgen/pipeline/unittest/conf.js
+docker exec -i descriptorgen protractor tng-sdk-descriptorgen/pipeline/unittest/conf.js
 
