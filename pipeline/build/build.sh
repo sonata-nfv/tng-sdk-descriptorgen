455e6fd20a1bfc8d8ddc1c098495f4d752ec7048
Stefan Schneider <stefan.schneider@upb.de>
2018-02-05 13:12:52 +0100

    build with --no-cache

--- pipeline/build/build.sh
+++ pipeline/build/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # todo: change to registry.sonata-nfv.eu:5000
-docker build -f DockerfilePipeline -t 5gtango/tng-sdk-descriptorgen:test .
+docker build --no-cache -f DockerfilePipeline -t 5gtango/tng-sdk-descriptorgen:test .
 
