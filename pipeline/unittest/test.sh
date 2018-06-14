8be31f949a47bc4980d1e686795c8f530fb89d4b
Stefan Schneider <stefan.schneider@upb.de>
2018-06-14 15:03:00 +0200

    wait for webdriver manager to finish updating (don't detach)

--- pipeline/unittest/test.sh
+++ pipeline/unittest/test.sh
@@ -8,8 +8,7 @@ docker rm -fv descriptorgen || true
 echo "Previous container(s) stopped and removed"
 
 docker run --name descriptorgen --privileged -i -d registry.sonata-nfv.eu:5000/tng-sdk-descriptorgen:test
-docker exec -i -d descriptorgen webdriver-manager update
-echo "Test container running and up-to-date"
-sleep 5
+docker exec -i descriptorgen webdriver-manager update
+echo "Webdriver manager updated, now starting unit tests"
 docker exec -i descriptorgen protractor tng-sdk-descriptorgen/pipeline/unittest/conf.js
 
