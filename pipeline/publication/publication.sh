db33c21ed2054681d6878940d3c557d6ef0fb246
Stefan Schneider <stefan.schneider@upb.de>
2018-09-27 09:07:49 +0200

    publish small deployment container, not huge testing container

--- pipeline/publication/publication.sh
+++ pipeline/publication/publication.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -e
+# override with smaller deployment docker container
+docker build -f Dockerfile -t registry.sonata-nfv.eu:5000/tng-sdk-descriptorgen .
 docker push registry.sonata-nfv.eu:5000/tng-sdk-descriptorgen
